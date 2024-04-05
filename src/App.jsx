29f613aa1ab0e382700f7f7c4d5e87d3cf1dbdfa
Amir Anvarkhujaev <84102683+AmirAnva@users.noreply.github.com>
2024-04-05 00:18:20 -0700

    finished product pushed to prod.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Stopwatch from './Stopwatch.jsx';
+
+function App() {
+  return (<>
+            <heading className = "heading">StopWatch.now</heading>
+            <br></br>
+            <Stopwatch />
+            <br></br>
+            <Stopwatch />
+            
+  </>);
+}
+
+export default App;
